2daad1ccb3edcc56ea5df208f55770c094198cd0
ugur.sahin <usahin19@ku.edu.tr>
2023-03-22 01:49:19 +0300

    In this modules, defined functions are checkPathway_Interactome_compatability function, which checks compatibility between a pathway and the interactome graph, creates a subgraph, and returns the subgraph along with any isolated nodes. Defines grandGraph_build function, which builds the interactome graph from interaction data, filtering by confidence if specified. Defines Pathway_parser function, which processes pathway data and returns a dictionary with pathway names as keys and lists of associated gene symbols as values. Reads interaction data (HIPPIE_dbs) and pathway data (CPDB_dbs) from external files. Defines drawGraph function, which saves the input graph as a GraphML file, and optionally visualizes the graph. Builds the grand interactome graph using the grandGraph_build function. Iterates through each pathway in the pathway data: a. Extracts the gene symbols for each pathway. b. Creates a subgraph for the pathway from the grand interactome graph. c. Checks compatibility of the subgraph with the interactome using the checkPathway_Interactome_compatability function. d. Draws and saves the compatible subgraph using the drawGraph function. Purpose of script processes interaction and pathway data to create compatible subgraphs for each pathway, saves them as GraphML(.GEXF) files based on HIPPIE and ConsensusPDB, and optionally visualizes the resulting graphs.

--- misc/network_pathway_integrator.py
+++ misc/network_pathway_integrator.py
@@ -1,12 +1,55 @@
 import pandas as pd
 import networkx as nx
+import matplotlib.pyplot as plt
 
-HIPPIE_dbs = pd.read_csv("../dbs/HIPPIE-2.2.mitab.txt",delimiter="\t")
-HIPPIE_dbs_filtered = HIPPIE_dbs[HIPPIE_dbs["Confidence Value"] >= 0.5]
 
-InteractionList = [(row["Gene Name Interactor A"], row["Gene Name Interactor B"]) for index,row in HIPPIE_dbs_filtered.iterrows()]
-G = nx.Graph(InteractionList)
-print(G)
+def checkPathway_Interactome_compatability(objectedPathway,G):
+    subGraph = nx.subgraph(G, objectedPathway) # Replace 'your_main_graph' with the main graph object
+    isolatedNodes = list(nx.isolates(subGraph))
 
-def grandGraphBuild(InteractionDBS):
-    pass
+    # Create a new graph object with the same nodes and edges as the subGraph
+    modifiable_subGraph = nx.Graph(subGraph)
+
+    # Remove isolated nodes from the new modifiable_subGraph
+    modifiable_subGraph.remove_nodes_from(isolatedNodes)
+
+    return modifiable_subGraph, isolatedNodes
+
+
+def grandGraph_build(InteractionDBS, filterConfidence=None):
+    if filterConfidence is not None:
+        InteractionDBS = InteractionDBS[InteractionDBS["Confidence Value"] >= filterConfidence]
+
+    InteractionList = [(row["Gene Name Interactor A"], row["Gene Name Interactor B"]) for index, row in
+                   InteractionDBS.iterrows()]
+
+    return nx.Graph(InteractionList)
+
+def Pathway_parser(PathwayDBS):
+    Pathway_defined_HugoSymb = dict()
+    for index,row in PathwayDBS.iterrows():
+        Pathway_defined_HugoSymb[row["pathway"]] = row["hgnc_symbol_ids"].split(",")
+    return Pathway_defined_HugoSymb
+
+HIPPIE_dbs, CPDB_dbs = pd.read_csv("../dbs/HIPPIE-2.2.mitab.txt",delimiter="\t"), \
+                       pd.read_csv("../dbs/CPDB_pathways_genes.tsv",delimiter="\t")
+
+def drawGraph(G,Pathway_name,saveGraphML=None, plot_show = None):
+    if saveGraphML is not None:
+        try:
+            nx.write_gexf(G,"../dbs/Graphlets/"+Pathway_name.replace(" ","_")+".gexf")
+        except:
+            print(Pathway_name)
+
+    if plot_show is not None:
+        nx.draw(G, with_labels=True, node_color='lightblue', font_weight='bold', node_size=500)
+        plt.show()
+
+if __name__ == '__main__':
+
+    grandGraph_overallPathways = grandGraph_build(HIPPIE_dbs)
+
+    for Pathway_name,HugoSymb_ls in Pathway_parser(CPDB_dbs).items():
+        objectedPathway = grandGraph_overallPathways.subgraph(HugoSymb_ls)
+        isolatedGraph_of_Pathway, isolatedNodes = checkPathway_Interactome_compatability(objectedPathway,grandGraph_overallPathways)
+        drawGraph(isolatedGraph_of_Pathway, Pathway_name,saveGraphML=True)
