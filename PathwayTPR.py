592cc22cd9d67ce38066fea64be8e8655022af55
ugur sahin <usahin19@ku.edu.tr>
2023-03-29 19:41:35 +0300

    The main is arranged/build based on the new changes. New argument parsers are added, but all pipeline doesn't ready to run anymore due to adding new indication models..

--- PathwayTPR.py
+++ PathwayTPR.py
@@ -3,6 +3,7 @@ import argparse
 from src.identifierSample import filterMutations, affected_pathway_finder
 from src.plotterChart import plot_as_radar_chart
 
+
 parser = argparse.ArgumentParser(description='Description of your program')
 parser.add_argument('--Sample', default=None, help='...')
 parser.add_argument('--Output', default=None, help='...')
@@ -13,15 +14,16 @@ parser.add_argument('--isDeleterious', default=False, action='store_true', help=
 parser.add_argument('--isCOSMIChotspot', default=False, action='store_true', help='...')
 parser.add_argument('--isTCGAhotspot', default=False, action='store_true', help='...')
 parser.add_argument('--justOncoPaths', default=False, action='store_false', help='...')
-parser.add_argument('--rawCountScore', default=False, action='store_false', help='...')
+#parser.add_argument('--rawCountScore', default=False, action='store_false', help='...')
 parser.add_argument('--differentiationIndicatorModel', default="Default", type=str, help='...')
+parser.add_argument('--scoreBaseModel', default=1, type=int, help='...')
 
 
 args = parser.parse_args()
 
 
 def main():
-    with open("/Users/ugur.sahin/PycharmProjects/TumorCharacterization/dbs/indexed_ConsensusPDB.json", "r") as indexed_ConsensusPBD_fl:
+    with open("dbs/indexed_ConsensusPDB.json", "r") as indexed_ConsensusPBD_fl:
         indexed_ConsensusPBD = json.load(indexed_ConsensusPBD_fl)
 
     with open(args.Sample,"r") as case_json_fl:
@@ -29,7 +31,9 @@ def main():
 
     filteredMutations = filterMutations(case_features["MutationProfile"],args.isDeleterious, args.isCOSMIChotspot, args.isTCGAhotspot)
 
-    counted_affected_path_dict = affected_pathway_finder(filteredMutations, rawCountScore=args.rawCountScore, differentiationIndicatorModel=args. differentiationIndicatorModel)
+    counted_affected_path_dict = affected_pathway_finder(filteredMutations,
+                                                         ScoreBaseModel=args.scoreBaseModel,
+                                                         differentiationIndicatorModel=args.differentiationIndicatorModel)
 
     plot_as_radar_chart(counted_affected_path_dict,args.maxNumberFeature, args.rawCountTreshold, args.showCaseInfo, args.Output)
 
