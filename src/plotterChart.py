0e1d778d67de158c0022984f24874e13b4b6feaa
ugur sahin <usahin19@ku.edu.tr>
2023-05-01 17:29:26 +0300

    Sample Names to denote in legends are edited properly under the plotRadarChart responsible to show single Sample Pathway features, and function names are changed.
    plotRadarChart_multipleSamples function is defined to plot multipleSample having Pathway Analysis.
    
    showPCA_basedPathway function is defined to show PCA output thrpugh rendering Matrix_of_alteredPathway using PCA function of sklearn, and responsible to plot it.

--- src/plotterChart.py
+++ src/plotterChart.py
@@ -1,19 +1,29 @@
+import random
 import warnings
 import pandas as pd
 import plotly.io as pio
 import plotly.express as px
+import plotly.colors as plc
+from sklearn.decomposition import PCA
+import numpy as np
+
+
 
 warnings.filterwarnings('ignore')
 
-def plot_as_radar_chart(disrupted_pathway_result_as_dct, maxNumberFeature, rawCountTreshold, showCaseInfo, Output):
+def plotRadarChart(disrupted_pathway_result_as_dct, maxNumberFeature, rawCountTreshold, Output):
     pathways, counts = list(),list()
     for k,v in disrupted_pathway_result_as_dct.items():
-        if rawCountTreshold is None or v >= rawCountTreshold:
-                pathways.append(k)
-                counts.append(v)
+        if rawCountTreshold is None or int(float(v)) >= rawCountTreshold:
+                try:
+                    pathways.append(k.replace(" - Homo sapiens (human)","- KEGG"))
+                except:
+                    pathways.append(k)
+
+                counts.append(float(v))
+
 
     be_plot_dbs = pd.DataFrame({"Pathways":pathways,"Disrupted_Score":counts})
-    #print(be_plot_dbs)
 
     try:
         fig = px.line_polar(be_plot_dbs, r = "Disrupted_Score", theta="Pathways",line_close=True )
@@ -29,19 +39,80 @@ def plot_as_radar_chart(disrupted_pathway_result_as_dct, maxNumberFeature, rawCo
         print("No Data remained to show ! because of harsh filter.")
         pass
 
-    """
-    note = "Trial: Trial<br>Trial: Trial"
+def plotRadarChart_multipleSamples(disruptedPathways, maxNumberFeature, rawCountTreshold, Output):
+    # Merge all pathways
+    mergedPathways = list()
+    for Sample in disruptedPathways.keys():
+        mergedPathways.extend(list(disruptedPathways[Sample].keys()))
+    mergedPathways = list(set(mergedPathways))
+
+    # Create a list of lists to store pathway values for each sample
+    collectionSample = list()
+    for Pathway in mergedPathways:
+        collectionPathway_tmp = list()
+        for Sample in disruptedPathways.keys():
+            try:
+                collectionPathway_tmp.append(np.log10(float(disruptedPathways[Sample][Pathway]) + 1))
+            except:
+                collectionPathway_tmp.append(0)
+
+        if any(disruptionScore > rawCountTreshold for disruptionScore in collectionPathway_tmp):
+            collectionPathway_tmp.insert(0, Pathway.replace(" - Homo sapiens (human)", "- KEGG"))
+            collectionSample.append(collectionPathway_tmp)
+        else:
+            pass
+
+    # Create dataframe with pathway values for each sample
+    columns = [Sample.replace(".json", "") for Sample in disruptedPathways.keys()]
+    columns.insert(0, "Pathway")
+    df = pd.DataFrame(collectionSample, columns=columns)
+
+    # Melt dataframe and apply log10 transformation to pathway values
+    df_melted = df.melt(id_vars=['Pathway'], var_name='Sample', value_name='Value')
+
+    # Plot pathway disruption comparison using polar line chart
+    fig = px.line_polar(df_melted, r='Value', theta='Pathway', color='Sample', line_close=True)
+    fig.update_traces(fill='toself')
+
     fig.update_layout(
-        annotations=[
-            dict(
-                x=1,
-                y=1,
-                xref="paper",
-                yref="paper",
-                text=note,
-                showarrow=False,
-                font=dict(size=14)
-            )
-        ]
+        title='Pathway Disruption Comparison - Log10Scale',
+        polar=dict(
+            radialaxis=dict(visible=True, type='log', tickformat='.2f'),
+            angularaxis=dict(visible=True)
+        ),
+        showlegend=True
     )
-    """
+
+    fig.show()
+
+
+def showPCA_basedPathway(Matrix_of_alteredPathway,plotName="defaultPCA.html"):
+    sample_names = list(Matrix_of_alteredPathway.columns)
+    groups = [sample.split("|")[0].split("_")[1] for sample in sample_names]
+
+    pca = PCA(n_components=2)
+    principal_components = pca.fit_transform(Matrix_of_alteredPathway.T)
+    principal_df = pd.DataFrame(data=principal_components, columns=['PC1', 'PC2'], index=sample_names)
+
+    # Sample isimlerini ve grupları DataFrame'e ekleyin
+    principal_df['Sample'] = sample_names
+    principal_df['Group'] = groups
+
+    # Renkleri rastgele ata
+    unique_groups = set(groups)
+    colors = {group: "#" + "".join([random.choice("0123456789ABCDEF") for j in range(6)]) for group in unique_groups}
+
+    # Scatter plot oluşturun ve renkleri özelleştirin
+    fig = px.scatter(principal_df, x='PC1', y='PC2', hover_name='Sample',
+                     color='Group', color_discrete_map=colors)
+
+    # Plot'u gösterin
+    fig.update_layout(title=plotName.split(".html")[0],width=1000, height=1000)
+    fig.write_html(plotName)
+    #fig.show()
+
+def get_value_by_substring(dictionary, substring):
+    for key, value in dictionary.items():
+        if substring in key:
+            return value
+    return None
