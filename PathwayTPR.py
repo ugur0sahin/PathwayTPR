9bdf13cbc6a898b2bfd17ae63078abe2156a6136
ugur sahin <usahin19@ku.edu.tr>
2023-05-01 17:20:00 +0300

    Structure of Body is arranged based on multiple Sample input for calculation and  plotting
    Pipeline is available to do PCA analyzes through using showReducedDimension attribution.
    
    Blocks are edited that handle sample names to be able to specify the legend and save it appropriately.

--- PathwayTPR.py
+++ PathwayTPR.py
@@ -1,40 +1,70 @@
 import json
+import random
 import argparse
-from src.identifierSample import filterMutations, affected_pathway_finder
-from src.plotterChart import plot_as_radar_chart
+import pandas as pd
+from src.identifierSample import filterMutations, affectedPathway_finder
+from src.plotterChart import plotRadarChart, plotRadarChart_multipleSamples, showPCA_basedPathway, get_value_by_substring
 
 
 parser = argparse.ArgumentParser(description='Description of your program')
-parser.add_argument('--Sample', default=None, help='...')
+parser.add_argument('--Sample',  nargs='+', default=None, help='...')
 parser.add_argument('--Output', default=None, help='...')
 parser.add_argument('--maxNumberFeature', default=None, type=int, help='...')
-parser.add_argument('--rawCountTreshold', default=None, type=int, help='...')
+parser.add_argument('--rawCountTreshold', default=0, type=float, help='...')
 parser.add_argument('--showCaseInfo', default=False, action='store_true', help='...')
 parser.add_argument('--isDeleterious', default=False, action='store_true', help='...')
 parser.add_argument('--isCOSMIChotspot', default=False, action='store_true', help='...')
 parser.add_argument('--isTCGAhotspot', default=False, action='store_true', help='...')
 parser.add_argument('--justOncoPaths', default=False, action='store_false', help='...')
-#parser.add_argument('--rawCountScore', default=False, action='store_false', help='...')
 parser.add_argument('--differentiationIndicatorModel', default="Default", type=str, help='...')
 parser.add_argument('--scoreBaseModel', default=1, type=int, help='...')
+parser.add_argument('--doSimilarity', default=False, action='store_false', help='...')
+parser.add_argument('--showReducedDimension', nargs='+',default="Default", type=str, help='...')
 
 
 args = parser.parse_args()
 
-
 def main():
 
-    with open(args.Sample,"r") as case_json_fl:
-        case_features = json.load(case_json_fl)
+    collectionSamples, indicatedMutations_ofSamples_of_definedPathways = dict(), dict()
+    for SamplePath in args.Sample:
+        try:
+            with open(SamplePath, "r") as case_json_fl:
+                case_features = json.load(case_json_fl)
+        except:
+            continue
 
-    filteredMutations = filterMutations(case_features["MutationProfile"],args.isDeleterious, args.isCOSMIChotspot, args.isTCGAhotspot)
+        filteredMutations = filterMutations(case_features["MutationProfile"], args.isDeleterious, args.isCOSMIChotspot,
+                                            args.isTCGAhotspot)
 
-    counted_affected_path_dict = affected_pathway_finder(filteredMutations,
-                                                         ScoreBaseModel=args.scoreBaseModel,
-                                                         differentiationIndicatorModel=args.differentiationIndicatorModel)
+        counted_affected_path_dict, definedMutations_toPathways = affectedPathway_finder(filteredMutations,
+                                                            ScoreBaseModel=args.scoreBaseModel,
+                                                            differentiationIndicatorModel=args.differentiationIndicatorModel), \
+                                                                  affectedPathway_finder(filteredMutations, justReturnPathways=True)
 
-    plot_as_radar_chart(counted_affected_path_dict,args.maxNumberFeature, args.rawCountTreshold, args.showCaseInfo, args.Output)
+        collectionSamples[str(case_features["CCLE_Name"])+"|"+str(case_features["Subtype_Disease"])], \
+            indicatedMutations_ofSamples_of_definedPathways[str(case_features["CCLE_Name"])+"|"+str(case_features["Subtype_Disease"])] =\
+            counted_affected_path_dict, definedMutations_toPathways
 
+        case_json_fl.close()
+
+    if len(collectionSamples.keys()) == 1:
+        plotRadarChart(collectionSamples[list(collectionSamples.keys())[0]], args.maxNumberFeature, args.rawCountTreshold, args.Output)
+
+    else:
+        plotRadarChart_multipleSamples(collectionSamples, args.maxNumberFeature, args.rawCountTreshold, args.Output)
+
+
+    if args.showReducedDimension != "Default":
+        from src.architectPathway import buildMatrix_alteredPathway_ofSamples
+        alterationMatrices_ofAllPathways_definedSamples = buildMatrix_alteredPathway_ofSamples(
+            indicatedMutations_ofSamples_of_definedPathways)
+
+        for Pathway in args.showReducedDimension:
+            print(Pathway)
+            #Matrix_of_alteredPathway = alterationMatrices_ofAllPathways_definedSamples[Pathway.replace("_"," ")]
+            Matrix_of_alteredPathway=get_value_by_substring(alterationMatrices_ofAllPathways_definedSamples, Pathway.replace("_"," "))
+            showPCA_basedPathway(Matrix_of_alteredPathway,plotName=Pathway+".PCA.html")
 
 
 if __name__ == '__main__':
