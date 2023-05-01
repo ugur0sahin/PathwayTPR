63fcede4cc0502ae88be46c7281292c1ab4ce99d
ugur sahin <usahin19@ku.edu.tr>
2023-05-01 16:37:56 +0300

    Pre-outputs are externalized using the alternative blocks depends on justReturnPathways, instead of continue pipes.
    Try|Except blocks are added into ScoreBaseModel 3 block to prevent broken pipeline in the unwanted cases.
    
    Unproper Notations and Names are changed.

--- src/identifierSample.py
+++ src/identifierSample.py
@@ -33,49 +33,66 @@ def filterMutations(MutationProfile,isDeleterious,isCOSMIChotspot,isTCGAhotspot)
 
     return nested_Mutations
 
-def affected_pathway_finder(MutationSet,justOncoPaths=False,ScoreBaseModel=1, differentiationIndicatorModel="Default"):
+def affectedPathway_finder(MutationSet,justOncoPaths=False,ScoreBaseModel=1, justReturnPathways=False ,differentiationIndicatorModel="Default"):
+    """Section1 -> Iterate All Mutations passed from the filtering to find it's related Pathways, add all Pathways into the
+    affectedPathways_forSample and do Pathways as keys to define related Mutations as list in the definedMutations_toPathways dictionary."""
     from src.scoreNetwork import main_disruption_rateGraph
 
     with open("dbs/indexed_ConsensusPDB.json", "r") as indexed_ConsensusPBD_fl:
         indexed_ConsensusPBD = json.load(indexed_ConsensusPBD_fl)
 
-    affected_pathways_for_sample, definedMutations_toPathways = list(), dict()
+    affectedPathways_forSample, definedMutations_toPathways = list(), dict()
     for Mutation in MutationSet:
         Gene = Mutation.split("_")[0]
         try:
             objectedMutation_ofPatway = indexed_ConsensusPBD[Gene]
-            affected_pathways_for_sample.extend(objectedMutation_ofPatway)
+            affectedPathways_forSample.extend(objectedMutation_ofPatway)
         except:
             continue
 
-        if ScoreBaseModel != 1:
-            from src.scoreNetwork import main_disruption_rateGraph
+        from src.scoreNetwork import main_disruption_rateGraph
 
-            for Pathway in objectedMutation_ofPatway:
-                if Pathway not in definedMutations_toPathways:
-                    definedMutations_toPathways[Pathway] = []
-                definedMutations_toPathways[Pathway].append(Gene)
+        for Pathway in objectedMutation_ofPatway:
+            if Pathway not in definedMutations_toPathways:
+                definedMutations_toPathways[Pathway] = []
+            definedMutations_toPathways[Pathway].append(Gene)
 
-    #print(affected_pathways_for_sample, definedMutations_toPathways)
+    """ This is the block which is the pathway provider into another pipelines, if program go in alternative way rather than scoring"""
+    if justReturnPathways:
+        return definedMutations_toPathways
+
+    """Section2 -> The affectedPathways_forSample and definedMutations_toPathways are constructed 
+    ScoreBaseModel 1 uses affectedPathways_forSample just counting the collected Mutations which impacts Pathways, 
+    the counts of Mutations belonged Pathways indicates degree of disruption. 
+     
+     ScoreBaseModel 2 and 3 are us Thoing definedMutations_toPathways dictionary contain <Pathway> : list(Mutations),
+     ScoreBaseModel 2 send dictionary into main_disruption_rateGraph(), ScoreBaseModel 3 send calculate_affectMutation_intoPathway()
+     
+     The expected output ->  Both of Them are <Pathway> : <DisruptionScore>
+     
+     """
     if ScoreBaseModel == 1: #WORKING!
-        return Counter(affected_pathways_for_sample)
+        return Counter(affectedPathways_forSample)
 
-    elif ScoreBaseModel == 2:#NOT WORKING YET!
+    elif ScoreBaseModel == 2: #NOT WORKING YET!
         return main_disruption_rateGraph(definedMutations_toPathways, differentiationIndicatorModel=differentiationIndicatorModel)
 
-    elif ScoreBaseModel == 3:
+    elif ScoreBaseModel == 3: #WORKING!
         from src.scoreNetwork import calculate_affectMutation_intoPathway
         Counter_Analog_dict = dict()
         for Pathway, MutationSet in definedMutations_toPathways.items():
-            returnedScore = calculate_affectMutation_intoPathway(Pathway, MutationSet)
-            print(Pathway,len(MutationSet) , returnedScore)
-            Counter_Analog_dict[Pathway] = returnedScore
-
+            try:
+                returnedScore = calculate_affectMutation_intoPathway(Pathway, MutationSet)
+                print(Pathway,len(MutationSet) , returnedScore)
+                Counter_Analog_dict[Pathway] = returnedScore
+            except:
+                pass
+        return Counter_Analog_dict
 """
 if __name__ != '__main__':
     with open("../example/ACH-000219.json","r") as case_json_fl:
         case_features = json.load(case_json_fl)
 
     filteredMutations = filterMutations(case_features["MutationProfile"],True,False,False)
-    print(affected_pathway_finder(filteredMutations))
+    print(affectedPathway_finder(filteredMutations))
 """
